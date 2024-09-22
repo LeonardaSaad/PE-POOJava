f665455d4d8be932751abf2bae44fa9c720124fc
Antonio Ramon <antonio-ramon-dev@outlook.com>
2024-09-22 08:26:59 -0300

    fix: register employee request response

--- Frontend/cadastroFuncionario.js
+++ Frontend/cadastroFuncionario.js
@@ -2,37 +2,41 @@ const form = document.querySelector("form");
 const inputName = document.querySelector(".iName");
 
 function cadastroFuncionario(obj) {
-  fetch("http://localhost:8080/funcionarios", {
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify(obj),
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((res) => {
-      console.log(res);
-    });
+	fetch("http://localhost:8080/funcionarios", {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify(obj),
+	})
+		.then((res) => {
+			if (!res.ok) throw new Error("deu erro mano!");
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 }
 
 function cleanForm() {
-  inputName.value = "";
+	inputName.value = "";
 }
 
 function handleForm(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  const dados = {
-    "name": inputName.value
-  };
-  console.log(dados);
+	const dados = {
+		name: inputName.value,
+	};
+	console.log(dados);
 
-  cadastroFuncionario(dados);
+	cadastroFuncionario(dados);
 
-  cleanForm();
+	cleanForm();
 }
 
 form.addEventListener("submit", handleForm);
